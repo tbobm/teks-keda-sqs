09b9e5183549ed7146650935a57de30acb51b000
Theo Bob Massard <tbobm@protonmail.com>
2021-06-02 11:32:48 +0200

    refactor: adapt template
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- terraform/live/backend/backend.tf
+++ terraform/live/backend/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 0.12.2"
 
   backend "s3" {
-    region         = "eu-west-1"
-    bucket         = "pio-teks-tf-state-lock"
+    region         = "eu-west-3"
+    bucket         = "tbobm-teks-keda-tf-state-lock"
     key            = "terraform.tfstate"
-    dynamodb_table = "pio-teks-tf-state-store-lock"
+    dynamodb_table = "tbobm-teks-keda-tf-state-store-lock"
     profile        = ""
     role_arn       = ""
     encrypt        = "true"
