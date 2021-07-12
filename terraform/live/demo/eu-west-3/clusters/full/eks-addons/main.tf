075410deddceee746f2dc3e0e55b6d6f564cd3c7
Theo Bob Massard <tbobm@protonmail.com>
2021-07-12 17:33:34 +0200

    feat(addons): upgrade to kubernetes-addons 2.6.0
    Adapt original eks-addons configuration
    
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- terraform/live/demo/eu-west-3/clusters/full/eks-addons/main.tf
+++ terraform/live/demo/eu-west-3/clusters/full/eks-addons/main.tf
@@ -61,32 +61,8 @@ module "eks-addons" {
     enabled = true
   }
 
-  calico = {
-    enabled = true
-  }
-
-  cert-manager = {
-    enabled                   = true
-    acme_email                = "kevin@particule.io"
-    acme_http01_enabled       = true
-    acme_http01_ingress_class = "nginx"
-    acme_dns01_enabled        = true
-    allowed_cidrs             = data.terraform_remote_state.vpc.outputs.vpc.private_subnets_cidr_blocks
-    experimental_csi_driver   = false
-  }
-
   cluster-autoscaler = {
     enabled = true
-  }
-
-  cni-metrics-helper = {
-    enabled = true
-  }
-
-  external-dns = {
-    external-dns = {
-      enabled = true
-    },
   }
 
   ingress-nginx = {
@@ -95,109 +71,14 @@ module "eks-addons" {
     allowed_cidrs = data.terraform_remote_state.vpc.outputs.vpc.private_subnets_cidr_blocks
   }
 
-  istio-operator = {
-    enabled = true
-  }
-
-  karma = {
-    enabled      = true
-    extra_values = <<-EXTRA_VALUES
-      ingress:
-        enabled: true
-        path: /
-        annotations:
-          kubernetes.io/ingress.class: nginx
-          cert-manager.io/cluster-issuer: "letsencrypt"
-        hosts:
-          - karma.${local.default_domain_suffix}
-        tls:
-          - secretName: karma.${local.default_domain_suffix}
-            hosts:
-              - karma.${local.default_domain_suffix}
-      env:
-        - name: ALERTMANAGER_URI
-          value: "http://kube-prometheus-stack-alertmanager.monitoring.svc.cluster.local:9093"
-        - name: ALERTMANAGER_PROXY
-          value: "true"
-        - name: FILTERS_DEFAULT
-          value: "@state=active severity!=info severity!=none"
-      EXTRA_VALUES
-  }
-
-  keycloak = {
-    enabled = true
-  }
-
-  kong = {
-    enabled = true
-  }
-
-  kube-prometheus-stack = {
-    enabled                     = true
-    allowed_cidrs               = data.terraform_remote_state.vpc.outputs.vpc.private_subnets_cidr_blocks
-    thanos_sidecar_enabled      = true
-    thanos_bucket_force_destroy = true
-    extra_values                = <<-EXTRA_VALUES
-      grafana:
-        deploymentStrategy:
-          type: Recreate
-        ingress:
-          enabled: true
-          annotations:
-            kubernetes.io/ingress.class: nginx
-            cert-manager.io/cluster-issuer: "letsencrypt"
-          hosts:
-            - grafana.${local.default_domain_suffix}
-          tls:
-            - secretName: grafana.${local.default_domain_suffix}
-              hosts:
-                - grafana.${local.default_domain_suffix}
-        persistence:
-          enabled: true
-          storageClassName: ebs-sc
-          accessModes:
-            - ReadWriteOnce
-          size: 1Gi
-      prometheus:
-        prometheusSpec:
-          replicas: 1
-          retention: 2d
-          retentionSize: "6GB"
-          ruleSelectorNilUsesHelmValues: false
-          serviceMonitorSelectorNilUsesHelmValues: false
-          podMonitorSelectorNilUsesHelmValues: false
-          storageSpec:
-            volumeClaimTemplate:
-              spec:
-                storageClassName: ebs-sc
-                accessModes: ["ReadWriteOnce"]
-                resources:
-                  requests:
-                    storage: 10Gi
-      EXTRA_VALUES
-  }
-
-  loki-stack = {
-    enabled              = true
-    bucket_force_destroy = true
+  keda = {
+    enabled   = true
+    create_ns = true
   }
 
   metrics-server = {
     enabled       = true
     allowed_cidrs = data.terraform_remote_state.vpc.outputs.vpc.private_subnets_cidr_blocks
-  }
-
-  npd = {
-    enabled = true
-  }
-
-  sealed-secrets = {
-    enabled = true
-  }
-
-  thanos = {
-    enabled              = true
-    bucket_force_destroy = true
   }
 
 }
